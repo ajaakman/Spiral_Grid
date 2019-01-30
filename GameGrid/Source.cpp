5888ad3eea0d72f0273b011d76fd691fbbf052ca
Artur Jaakman <jaakmanartur@gmail.com>
2019-01-30 20:16:35 +0200

    Update Source.cpp

--- GameGrid/Source.cpp
+++ GameGrid/Source.cpp
@@ -11,15 +11,15 @@
 #define WEST 6
 #define NORTHWEST 7
 
-class Grid
+class SpiralGrid
 {
 public:
 
-	Grid(int s)
+	SpiralGrid(int s)
 	{
 		coordinates = GenerateGrid(s);
 	};
-	~Grid() {};
+	~SpiralGrid() {};
 
 
 	//int* AdjesentLocation(const int& loc, const int& dir)
@@ -40,10 +40,18 @@ public:
 
 	void PrintGrid()
 	{
-		for (auto & coord : coordinates)
+		for (int i = 0; i < coordinates.size(); i++)
 		{
-			std::cout << coord.first << " " << coord.second << std::endl;
+			std::cout << i << " " <<  coordinates[i].first << " " << coordinates[i].second << std::endl;
 		}
+	}
+
+	std::pair<int, int> GetCoordAtIndex(int index)
+	{		
+		std::pair<int, int> coord = { 0, 0 };
+
+		
+		return coord;
 	}
 
 private:
@@ -54,44 +62,70 @@ private:
 		coordinates.resize(s * s);
 		coordinates[0] = { 0, 0 };
 
-		int inc = 1, times = 1, b = times;
-		bool which = false;
+		int sign = 1;
+		bool dir = false;
 
 		for (unsigned int i = 1; i < coordinates.size(); ++i)
 		{
-			if (which)	
-				coordinates[i] = { coordinates[i - 1].first += inc, coordinates[i - 1].second };
+			if (dir)
+				coordinates[i] = { coordinates[i - 1].first += sign, coordinates[i - 1].second };
 			else
-				coordinates[i] = { coordinates[i - 1].first, coordinates[i - 1].second += inc };
+				coordinates[i] = { coordinates[i - 1].first, coordinates[i - 1].second += sign };
 
-			b--;
-
-			if (b == 0) // i == 0, 1, 2, 4, 6, 9, 12, 16, 20
+			if (isSpiral(i)) // i == 0, 1, 2, 4, 6, 9, 12, 16, 20, 25, 30
 			{
-				if (which) // i == 0, 2, 6, 12, 20
+				if (isSpiral2(i)) // i == 0, 2, 6, 12, 20, 30
 				{
-					which = false;
-					inc *= -1;
-					times++;
+					dir = false;
+					sign *= -1;
 				}	
 				else
-					which = true;
-				
-				b = times;
+				{
+					dir = true;
+				}				
 			}
 		}
 
 		return coordinates;
 	}
 
+	bool isSpiral(int x)
+	{
+		return (ceil(sqrt(4 * x)) != ceil(sqrt(4 * (x + 1))));
+	}
+
+	bool isSpiral2(int x)
+	{
+		return (round(sqrt(x)) != round(sqrt(x + 1)));
+	}
 };
+
+
+
 
 int main()
 {
-	Grid g(5);
+	SpiralGrid g(11);
 	g.PrintGrid();
 
-	std::cout << g.GetGridSize() << std::endl;
+	//std::cout << "At index 4: "<<(g.GetCoordAtIndex(4).first) << " " << (g.GetCoordAtIndex(4).second) << std::endl;
+
+	//for (int x = 0; x < 20; x++)
+	//	std::cout << ( round(pow(x,2) / 4) ) << std::endl;
+
+	//for (int x = 0; x < 40; x++)
+	//	std::cout << round(sqrt(1 * x)) << "   " << x << std::endl;
+
+	//for (int x = 0; x < 40; x++)
+	//{
+	//	if (isInteger1(x))
+	//		std::cout << x << std::endl;
+	//}
+		
+
+	//std::cout << isPerfectSquare(12) << std::endl;
+
+
 	
 	//std::cout << *(g.AdjesentLocation(0, NORTH)) << std::endl;
 
